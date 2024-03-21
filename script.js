bdd19a5460536936f197e47238e9d309b6e68781
Joydeep Sarkar <138491246+joydeepsarkar99@users.noreply.github.com>
2024-03-21 11:47:09 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 //your JS code here. If required.
+let nameInput = document.querySelector("#fname")
+nameInput.addEventListener("blur",changeName)
+function changeName(){
+    let user = nameInput.value 
+    nameInput.value = user.toUpperCase()
+}
